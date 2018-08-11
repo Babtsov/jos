21c7a1ad45083cb8b1c2e0590ce2c3eb0a990487
Babtsov <ben.babtsov@ufl.edu>
2018-08-11 08:42:34 +0000

    [JOS] lab 6 exercise 1

--- kern/trap.c
+++ kern/trap.c
@@ -247,6 +247,7 @@ trap_dispatch(struct Trapframe *tf)
 	// LAB 4: Your code here.
 	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
 		lapic_eoi();
+		time_tick();
 		sched_yield(); // noreturn
 	}
 
