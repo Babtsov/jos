efe5456a1062d2cce31cca5eb05d8e21bf72c2c8
Babtsov <ben.babtsov@ufl.edu>
2018-08-12 06:13:55 +0000

    [JOS] lab 6 exercise 3

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,9 @@
 #include <kern/e1000.h>
+#include <kern/pci.h>
+#include <inc/stdio.h>
 
 // LAB 6: Your driver code here
+int e1000_attach(struct pci_func *pcif) {
+	pci_func_enable(pcif);
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,7 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+#include <kern/pci.h>
+
+int e1000_attach(struct pci_func *pcif);
 
 #endif	// JOS_KERN_E1000_H
